1a68863a1d3900c506c89ede7af6b3c5d9e4e059
gwanyongpark <rhksdyd12312@gmail.com>
2023-06-20 17:40:51 +0900

    2023-06-20

--- BootEx/b4/src/main/java/com/example/b4/mappers/ProductMapper.java
+++ BootEx/b4/src/main/java/com/example/b4/mappers/ProductMapper.java
@@ -1,12 +1,18 @@
 package com.example.b4.mappers;
 
 import java.util.List;
+import java.util.Map;
 
 import com.example.b4.dto.PageRequestDTO;
 import com.example.b4.dto.ProductListDTO;
+import com.example.b4.dto.ProductRegisterDTO;
 
 public interface ProductMapper {
-    
-    List<ProductListDTO> getList(PageRequestDTO pageRequestDTO);
+  
+  List<ProductListDTO> getList(PageRequestDTO pageRequestDTO);
+
+  int insertProduct(ProductRegisterDTO registerDTO);
+
+  int insertImages(List<Map<String,String>> imageList);
 
 }
